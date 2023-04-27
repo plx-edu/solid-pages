6ee88912658969d2d339093734ccd99b4cc35429
plx-edu <60093850+plx-edu@users.noreply.github.com>
2023-04-27 14:48:30 +0000

    Deploying to gh-pages from @ plx-edu/solid-pages@49ff22ae85c5e6d893cd1234f8c5c1ad8d1f0552 🚀

--- assets/layout-b8964c3f.js
+++
@@ -1,1 +0,0 @@
-import{g as t,t as e}from"./entry-client-b500d5e1.js";const a=e('<main class="text-center mx-auto text-gray-700 p-4"><h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">Layout Page</h1><p class="mt-8">[Insert content here]');function n(){return t(a)}export{n as default};
