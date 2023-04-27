a362e6c82b71a0249e4860e021137a59f29974e5
plx-edu <plx-edu@hotmail.com>
2023-04-27 16:54:45 +0200

    update: styling

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -10,7 +10,7 @@ export default function Layout() {
         [Insert content here]
       </p>
 
-      <div class="w-5">
+      <div class="m-auto w-40">
       <Square>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width={1.5} stroke="currentColor" class="w-6 h-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
