af0816cbe272c51d438f3fcc58a97fc66b3b367b
plx-edu <plx-edu@hotmail.com>
2023-04-29 14:26:41 +0200

    dev: bg-img & hover for archive purpose

--- src/components/AddButton.tsx
+++ src/components/AddButton.tsx
@@ -1,13 +1,21 @@
 export default function AddButton(props: {children: any, bgUrl?: string}) {
   return (
     <button class="flex justify-center items-center
-      w-full h-full
-      hover:bg-gradient-to-b from-slate-900 to-slate-200
-      bg-cover bg-center"
-      style={props.bgUrl ? `background-image: url('${props.bgUrl}')` : ""}
+      w-full h-full relative
+      bg-center bg-cover bg-no-repeat"
+      style={`background-image: ${props.bgUrl ? `url('${props.bgUrl}')` : ""}`}
     >
-      {/* classList={{ `bg-[url('${props.bgUrl ? "" : ""}')]` : props.bgUrl ? true : false }} */}
-      {props.children}
+      <div class="absolute
+        w-full h-full
+        flex justify-center items-center
+        hover:bg-gradient-to-b from-slate-900 to-slate-200
+        opacity-10"
+      >
+        {/* {props.bgUrl ? <img class="absolute w-full h-full -z-0" src={`${props.bgUrl }`} alt="" /> : <></>} */}
+      </div>
+
+        {/* classList={{ `bg-[url('${props.bgUrl ? "" : ""}')]` : props.bgUrl ? true : false }} */}
+        {props.children}
     </button>
   );
 }
