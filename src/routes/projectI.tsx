c2fe263afe6e5606cbaf2f725651eb4b39131062
plx-edu <plx-edu@hotmail.com>
2023-11-18 22:51:58 +0100

    cell color update

--- src/routes/projectI.tsx
+++ src/routes/projectI.tsx
@@ -4,13 +4,39 @@ import { createContext, createEffect, createSignal, on, useContext } from "solid
 const boxPadding = "p-0.5";
 
 export default function projectI() {
-  const arr = [...Array(9).keys()];
+  const [gridSize, setGridSize] = createSignal(9);
+  const arr = [...Array(gridSize()).keys()];
+  // const arr = [...Array(9).keys()];
 
   const Cube = () => {
-    const [isActive, setIsActive] = createSignal(false);
+    const [n, setN] = createSignal(0)
+    const [cellColor, setCellColor] = createSignal("bg-slate-200")
+
+    function setColor(){
+      setN(n() + 1);
+
+      switch(n()){
+        case 1:
+          setCellColor("bg-slate-500");
+          break;
+        case 2:
+            setCellColor("bg-indigo-200");
+            break;
+        case 3:
+          setCellColor("bg-indigo-300");
+          break;
+        default:
+          setN(0);
+          setCellColor("bg-slate-200");
+          break;
+      }
+    }
+
+
+
     return (
-      <button class={`w-full h-full ${isActive() ? "bg-slate-500" : "bg-slate-200" }`} 
-        ondblclick={() => setIsActive(!isActive())}
+      <button class={`w-full h-full ${cellColor()}`} 
+        ondblclick={() => setColor()}
       >
       </button>
     )
@@ -21,7 +47,7 @@ export default function projectI() {
     <div class="flex flex-row">
       { arr.map((_item, index) => {
         return (
-          <Square innerClass={boxPadding} key={index} isRatioVisible={true}>
+          <Square innerClass={boxPadding} key={index} isRatioVisible={false}>
             <Cube></Cube>
           </Square>
         )
@@ -31,6 +57,7 @@ export default function projectI() {
   }
 
   return (
+    <>
     <div id="here" class="h-screen w-full flex justify-center items-center">
       <Square innerClass={boxPadding} isRatioVisible={false}>
         { arr.map((_item, index) => {
@@ -38,5 +65,10 @@ export default function projectI() {
         })}
       </Square>
     </div>
+    {/* <div class="bg-slate-400">
+      <input type="number" name="gridSizeSetter" id="" min={3} max={12}
+      onChange={e => setGridSize(Number(e.target.value))}/>
+    </div> */}
+    </>
   );
 }
